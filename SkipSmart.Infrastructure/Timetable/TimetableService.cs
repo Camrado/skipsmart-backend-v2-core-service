f0287fb32baf27fc6cf0bec507c68b9af1f0f7bd
Camrado <blackandwhite.b17w@gmail.com>
2024-09-11 16:37:02 +0400

    Fixed the logic behind getting unmarked dates

--- SkipSmart.Infrastructure/Timetable/TimetableService.cs
+++ SkipSmart.Infrastructure/Timetable/TimetableService.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Options;
 using SkipSmart.Application.Abstractions.Timetable;
 using SkipSmart.Application.Attendances.GetTimetableForGroup;
 using SkipSmart.Domain.Abstractions;
+using SkipSmart.Domain.Courses;
 using SkipSmart.Domain.Groups;
 using SkipSmart.Domain.Users;
 
@@ -25,13 +26,15 @@ internal sealed class TimetableService : ITimetableService {
     private readonly HttpClient _httpClient;
     private readonly IUserRepository _userRepository;
     private readonly IGroupRepository _groupRepository;
+    private readonly ICourseRepository _courseRepository;
     private readonly string _key;
     private readonly string _baseUrl;
     
-    public TimetableService(HttpClient httpClient, IGroupRepository groupRepository, IUserRepository userRepository) {
+    public TimetableService(HttpClient httpClient, IGroupRepository groupRepository, IUserRepository userRepository, ICourseRepository courseRepository) {
         _httpClient = httpClient;
         _userRepository = userRepository;
         _groupRepository = groupRepository;
+        _courseRepository = courseRepository;
         _key = Environment.GetEnvironmentVariable("TIMETABLE_API_KEY") 
                ?? throw new ApplicationException("Timetable API key is missing");
         _baseUrl = Environment.GetEnvironmentVariable("TIMETABLE_API_BASE_URL") 
@@ -72,21 +75,24 @@ internal sealed class TimetableService : ITimetableService {
         try {
             var user = await _userRepository.GetByIdAsync(userId, cancellationToken);
             var group = await _groupRepository.GetByIdAsync(user.GroupId, cancellationToken);
+            var courses = (await _courseRepository.GetAllByGroupIdAsync(user.GroupId, cancellationToken)).Select(c => c.CourseName.Value);
+            string coursesParam = string.Join(";", courses);
 
-            var queryParams = HttpUtility.ParseQueryString(string.Empty);
-            queryParams["key"] = _key;
-            queryParams["group_id"] = group.EdupageClassId.ToString();
-            queryParams["language_subgroup"] = user.LanguageSubgroup.ToString(); 
-            queryParams["faculty_subgroup"] = user.FacultySubgroup.ToString();
-            queryParams["start_date"] = startDate.ToString("yyyy-MM-dd");
-            queryParams["end_date"] = endDate.ToString("yyyy-MM-dd");
+            var requestBody = new {
+                key = _key,
+                group_id = group.EdupageClassId.ToString(),
+                language_subgroup = user.LanguageSubgroup.ToString(),
+                faculty_subgroup = user.FacultySubgroup.ToString(),
+                start_date = startDate.ToString("yyyy-MM-dd"),
+                end_date = endDate.ToString("yyyy-MM-dd"),
+                courses = coursesParam
+            };  
         
-            var response = await _httpClient.GetAsync(
-                $"{_baseUrl}/working-days?{queryParams}", cancellationToken);
+            var response = await _httpClient.PostAsJsonAsync($"{_baseUrl}/working-days", requestBody, cancellationToken);
             response.EnsureSuccessStatusCode();
         
             var responseData = await response.Content.ReadFromJsonAsync<IReadOnlyList<DateOnly>>(cancellationToken);
-        
+
             if (responseData is null) {
                 return Result.Failure<IReadOnlyList<DateOnly>>(WorkingDaysRequestFailed);
             }
